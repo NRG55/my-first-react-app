33683392bcdd4cd57207fb41d8bac48f23e4f0e0
NRG55 <maloj_1980@yahoo.fr>
2025-05-09 20:40:08 +0100

    Add: practice-state (State-introduction.jsx)

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,23 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './App.jsx';
-import Greeting from './Greetings.jsx';
-import AnimalList from './practice-rendering-techniques/RerenderTechniques.jsx';
-import PackingList from './practice-conditional-rendering/PackingList.jsx';
-import { List4 } from './practice-rendering-lists/RenderingLists.jsx';
-import { RecipeList2 } from './practice-rendering-lists/RenderingLists.jsx';
-import { Poem2 } from './practice-rendering-lists/RenderingLists.jsx';
-import { TodoList } from './practice-using-keys/UsingKeys.jsx';
-import { RenderButtons } from './practice-passing-data-between-components/PassingData.jsx';
-import { Profile4 } from './practice-passing-data-between-components/PassingData.jsx';
-import { GalleryV2 } from './practice-passing-data-between-components/PassingData.jsx';
-import { ProfileV3 } from './practice-passing-data-between-components/PassingData.jsx';
 import './index.css';
+// import App from './App.jsx';
+// import Greeting from './Greetings.jsx';
+// import AnimalList from './practice-rendering-techniques/RerenderTechniques.jsx';
+// import PackingList from './practice-conditional-rendering/PackingList.jsx';
+// import { List4 } from './practice-rendering-lists/RenderingLists.jsx';
+// import { RecipeList2 } from './practice-rendering-lists/RenderingLists.jsx';
+// import { Poem2 } from './practice-rendering-lists/RenderingLists.jsx';
+// import { TodoList } from './practice-using-keys/UsingKeys.jsx';
+// import { RenderButtons } from './practice-passing-data-between-components/PassingData.jsx';
+// import { Profile4 } from './practice-passing-data-between-components/PassingData.jsx';
+// import { GalleryV2 } from './practice-passing-data-between-components/PassingData.jsx';
+// import { ProfileV3 } from './practice-passing-data-between-components/PassingData.jsx';
+// import { GalleryState2 } from './practice-state/State';
+// import { Form } from './practice-state/State';
+// import { FeedbackForm } from './practice-state/State';
+import { FeedbackForm2 } from './practice-state/State-introduction';
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
@@ -26,6 +31,10 @@ createRoot(document.getElementById('root')).render(
     {/* < RenderButtons /> */}
     {/* <Profile4 /> */}
     {/* <GalleryV2 /> */}
-    <ProfileV3 />
+    {/* <ProfileV3 /> */}
+    {/* <GalleryState2 />     */}
+    {/* <Form /> */}
+    {/* <FeedbackForm /> */}
+    <FeedbackForm2 />
   </StrictMode>,
 )
---
+++ src/practice-state/State-introduction.jsx
@@ -0,0 +1,347 @@
+import { useState } from 'react';
+
+const sculptureList = [{
+  name: 'Homenaje a la Neurocirugía',
+  artist: 'Marta Colvin Andrade',
+  description: 'Although Colvin is predominantly known for abstract themes that allude to pre-Hispanic symbols, this gigantic sculpture, an homage to neurosurgery, is one of her most recognizable public art pieces.',
+  url: 'https://i.imgur.com/Mx7dA2Y.jpg',
+  alt: 'A bronze statue of two crossed hands delicately holding a human brain in their fingertips.'  
+}, {
+  name: 'Floralis Genérica',
+  artist: 'Eduardo Catalano',
+  description: 'This enormous (75 ft. or 23m) silver flower is located in Buenos Aires. It is designed to move, closing its petals in the evening or when strong winds blow and opening them in the morning.',
+  url: 'https://i.imgur.com/ZF6s192m.jpg',
+  alt: 'A gigantic metallic flower sculpture with reflective mirror-like petals and strong stamens.'
+}, {
+  name: 'Eternal Presence',
+  artist: 'John Woodrow Wilson',
+  description: 'Wilson was known for his preoccupation with equality, social justice, as well as the essential and spiritual qualities of humankind. This massive (7ft. or 2,13m) bronze represents what he described as "a symbolic Black presence infused with a sense of universal humanity."',
+  url: 'https://i.imgur.com/aTtVpES.jpg',
+  alt: 'The sculpture depicting a human head seems ever-present and solemn. It radiates calm and serenity.'
+}];
+
+export function GalleryState() {
+  const [index, setIndex] = useState(0);
+
+  function handleClick() {
+    setIndex(index + 1);
+  }
+
+  let sculpture = sculptureList[index];
+  return (
+    <>
+      <button onClick={handleClick}>
+        Next
+      </button>
+      <h2>
+        <i>{sculpture.name} </i> 
+        by {sculpture.artist}
+      </h2>
+      <h3>  
+        ({index + 1} of {sculptureList.length})
+      </h3>
+      <img 
+        src={sculpture.url} 
+        alt={sculpture.alt}
+      />
+      <p>
+        {sculpture.description}
+      </p>
+    </>
+  );
+}
+
+// This example doesn’t use React but it gives an idea of how useState works internally:
+//--------------------------------------------------
+let componentHooks = [];
+let currentHookIndex = 0;
+
+// How useState works inside React (simplified).
+function useState2(initialState) {
+  let pair = componentHooks[currentHookIndex];
+  if (pair) {
+    // This is not the first render,
+    // so the state pair already exists.
+    // Return it and prepare for next Hook call.
+    currentHookIndex++;
+    return pair;
+  }
+
+  // This is the first time we're rendering,
+  // so create a state pair and store it.
+  pair = [initialState, setState];
+
+  function setState(nextState) {
+    // When the user requests a state change,
+    // put the new value into the pair.
+    pair[0] = nextState;
+    updateDOM();
+  }
+
+  // Store the pair for future renders
+  // and prepare for the next Hook call.
+  componentHooks[currentHookIndex] = pair;
+  currentHookIndex++;
+  return pair;
+}
+//------------------------------------------------
+
+// =================CHALLENGES==================
+// Challenge 1 - Complete the gallery: When you press “Next” on the last sculpture, 
+//                                     the code crashes. Fix the logic to prevent the crash
+export function ExampleGallery() {
+  const [index, setIndex] = useState(0);
+  const [showMore, setShowMore] = useState(false);
+
+  function handleNextClick() {
+    setIndex(index + 1);
+  }
+
+  function handleMoreClick() {
+    setShowMore(!showMore);
+  }
+
+  let sculpture = sculptureList[index];
+  return (
+    <>
+      <button onClick={handleNextClick}>
+        Next
+      </button>
+      <h2>
+        <i>{sculpture.name} </i> 
+        by {sculpture.artist}
+      </h2>
+      <h3>  
+        ({index + 1} of {sculptureList.length})
+      </h3>
+      <button onClick={handleMoreClick}>
+        {showMore ? 'Hide' : 'Show'} details
+      </button>
+      {showMore && <p>{sculpture.description}</p>}
+      <img 
+        src={sculpture.url} 
+        alt={sculpture.alt}
+      />
+    </>
+  );
+}
+
+// Solution
+export function GalleryState2() {
+  const [index, setIndex] = useState(0);
+  const [showMore, setShowMore] = useState(false);
+
+  let hasPrev = index > 0;
+  let hasNext = index < sculptureList.length - 1;
+
+  function handlePrevClick() {
+    if (hasPrev) {
+      setIndex(index - 1);
+    };
+  }
+
+  function handleNextClick() {
+    if (hasNext) {
+      setIndex(index + 1);
+    };
+  }
+
+  function handleMoreClick() {
+    setShowMore(!showMore);
+  }
+
+  let sculpture = sculptureList[index];
+
+  return (
+    <>
+      <button
+        onClick={handlePrevClick}
+        disabled={!hasPrev}
+      >
+        Previous
+      </button>
+      <button
+        onClick={handleNextClick}
+        disabled={!hasNext}
+      >
+        Next
+      </button>
+      <h2>
+        <i>{sculpture.name} </i> 
+        by {sculpture.artist}
+      </h2>
+      <h3>  
+        ({index + 1} of {sculptureList.length})
+      </h3>
+      <button onClick={handleMoreClick}>
+        {showMore ? 'Hide' : 'Show'} details
+      </button>
+      {showMore && <p>{sculpture.description}</p>}
+      <img 
+        src={sculpture.url} 
+        alt={sculpture.alt}
+      />
+    </>
+  );
+}
+// hasPrev and hasNext are used both for the returned JSX and inside the event 
+// handlers. This handy pattern works because event handler functions “close over”
+// any variables declared while rendering
+
+// Challenge 2 - Fix stuck form inputs
+export function ExampleForm() {
+  let firstName = '';
+  let lastName = '';
+
+  function handleFirstNameChange(e) {
+    firstName = e.target.value;
+  }
+
+  function handleLastNameChange(e) {
+    lastName = e.target.value;
+  }
+
+  function handleReset() {
+    firstName = '';
+    lastName = '';
+  }
+
+  return (
+    <form onSubmit={e => e.preventDefault()}>
+      <input
+        placeholder="First name"
+        value={firstName}
+        onChange={handleFirstNameChange}
+      />
+      <input
+        placeholder="Last name"
+        value={lastName}
+        onChange={handleLastNameChange}
+      />
+      <h1>Hi, {firstName} {lastName}</h1>
+      <button onClick={handleReset}>Reset</button>
+    </form>
+  );
+}
+
+// Solution
+export function Form() {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+
+  function handleFirstNameChange(e) {    
+    setFirstName(e.target.value);  
+  }
+
+  function handleLastNameChange(e) {
+    setLastName(e.target.value);
+  }
+
+  function handleReset() {
+    setFirstName("");
+    setLastName("");
+  }
+
+  return (
+    <form onSubmit={e => e.preventDefault()}>
+      <input
+        placeholder="First name"
+        value={firstName}
+        onChange={handleFirstNameChange}
+      />
+      <input
+        placeholder="Last name"
+        value={lastName}
+        onChange={handleLastNameChange}
+      />
+      <h1>Hi, {firstName} {lastName}</h1>
+      <button onClick={handleReset}>Reset</button>
+    </form>
+  );
+}
+
+// Challenge 3 - Fix a crash
+export function ExampleFeedbackForm() {
+  const [isSent, setIsSent] = useState(false);
+  if (isSent) {
+    return <h1>Thank you!</h1>;
+  } else {
+    // eslint-disable-next-line
+    const [message, setMessage] = useState('');
+    return (
+      <form onSubmit={e => {
+        e.preventDefault();
+        alert(`Sending: "${message}"`);
+        setIsSent(true);
+      }}>
+        <textarea
+          placeholder="Message"
+          value={message}
+          onChange={e => setMessage(e.target.value)}
+        />
+        <br />
+        <button type="submit">Send</button>
+      </form>
+    );
+  };
+}
+
+// Solution
+export function FeedbackForm() {
+  const [isSent, setIsSent] = useState(false);
+  const [message, setMessage] = useState('');
+
+  if (isSent) {
+    return <h1>Thank you!</h1>;
+  };
+
+  return (
+    <form onSubmit={e => {
+      e.preventDefault();
+      alert(`Sending: "${message}"`);
+      setIsSent(true);
+    }}>
+      <textarea
+        placeholder="Message"
+        value={message}
+        onChange={e => setMessage(e.target.value)}
+      />
+      <br />
+      <button type="submit">Send</button>
+    </form>
+  );
+}
+
+// Challenge 4 - Remove unnecessary state
+export function ExampleFeedbackForm2() {
+  const [name, setName] = useState('');
+
+  function handleClick() {
+    setName(prompt('What is your name?'));
+    alert(`Hello, ${name}!`);
+  }
+
+  return (
+    <button onClick={handleClick}>
+      Greet
+    </button>
+  );
+}
+
+// Solution
+export function FeedbackForm2() {
+  function handleClick() { 
+    const name = prompt('What is your name?');     
+    alert(`Hello, ${name}!`);   
+  };
+
+  return (
+    <button onClick={handleClick}>
+      Greet
+    </button>
+  );
+}
+
+
+
+
