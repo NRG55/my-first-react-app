3a3e56f32b1c46f156fc68ea177be0c076337dbf
NRG55 <maloj_1980@yahoo.fr>
2025-06-08 17:25:30 +0100

    Create: practice-testing. Install: vitest, jsdom, react testing-library/jest-dom and testing-library/user-event. Create(tests): setup.js

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: '/src/practice-testing/tests/setup.js'
+  },
+});
